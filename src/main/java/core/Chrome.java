bb437a99906e3880f3c31473f9edb6b4af5c160a
Andrei Sobolev <s_bizon@outlook.com>
2017-11-01 21:33:41 -0700

    initial commit

--- src/main/java/core/Chrome.java
+++ src/main/java/core/Chrome.java
@@ -1,16 +1,19 @@
 package core;
-
  
-
 import org.openqa.selenium.*;
-
 import org.openqa.selenium.chrome.*;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.*;
+
+
 
 import org.openqa.selenium.support.ui.*;
 
-import java.util.concurrent.TimeUnit;
 
-import java.util.logging.*;
+
+
+
+
 
  
 public class Chrome {
@@ -27,9 +30,9 @@ public class Chrome {
  
 
               String url = "http://facebook.com/";
-              String email_address = "***************";
+              String email_address = "***********";
 
-              String password = "*************";
+              String password = "***********";
 
 
               if (System.getProperty("os.name").toUpperCase().contains("MAC"))      driverPath = "./resources/webdrivers/mac/chromedriver";
@@ -63,29 +66,58 @@ public class Chrome {
 
                      wait.until(ExpectedConditions.titleIs("Facebook - Log In or Sign Up"));
 
-                     String title = driver.getTitle();
+ String title = driver.getTitle();
+                     
+                     String copyright = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\'pageFooter\']/div[3]/div/span"))).getText();
 
-                    
+                     System.out.println("Size of copyright: " + (Dimension) driver.findElement(By.xpath("//*[@id=\'pageFooter\']/div[3]/div/span")).getSize());
 
-String copyright = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\'pageFooter\']/div[3]/div/span"))).getText();
+                     System.out.println("Location of copyright: " + (Point) driver.findElement(By.xpath("//*[@id=\'pageFooter\']/div[3]/div/span")).getLocation());
+                     wait.until(ExpectedConditions.presenceOfElementLocated(By.id("email"))).clear();
 
-wait.until(ExpectedConditions.presenceOfElementLocated(By.id("email"))).clear();
+                     wait.until(ExpectedConditions.presenceOfElementLocated(By.id("email"))).sendKeys(email_address);
+                     
+                     System.out.println("Size of Email: " + (Dimension) driver.findElement(By.id("email")).getSize());
 
-wait.until(ExpectedConditions.presenceOfElementLocated(By.id("email"))).sendKeys(email_address);
+                     System.out.println("Location of Email: " + (Point) driver.findElement(By.id("email")).getLocation());
+                     
+                     wait.until(ExpectedConditions.presenceOfElementLocated(By.id("pass"))).clear();
 
-wait.until(ExpectedConditions.presenceOfElementLocated(By.id("pass"))).clear();
+                     wait.until(ExpectedConditions.presenceOfElementLocated(By.id("pass"))).sendKeys(password);
+                     
+                     System.out.println("Size of Size of Password: " + (Dimension) driver.findElement(By.id("pass")).getSize());
 
-wait.until(ExpectedConditions.presenceOfElementLocated(By.id("pass"))).sendKeys(password);
+                     System.out.println("Location of Size of Password: " + (Point) driver.findElement(By.id("pass")).getLocation());
 
-wait.until(ExpectedConditions.elementToBeClickable(By.id("u_0_2"))).click();
+                     wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"u_0_2\"]"))).click();
+                     
+                     System.out.println("Size of Size of Login: " + (Dimension) driver.findElement(By.xpath("//*[@id=\"u_0_2\"]")).getSize());
 
-wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"u_0_a\"]/div[1]/div[1]/div/a/span/span"))).click();
+                     System.out.println("Location of Size of Login: " + (Point) driver.findElement(By.xpath("//*[@id=\"u_0_2\"]")).getLocation());
 
-String friends = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[2]/ul/li[3]/a/span[1]"))).getText();      
+                     wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='u_0_a']/div[1]/div[1]/div/a/span"))).click();
 
-wait.until(ExpectedConditions.presenceOfElementLocated(By.id("userNavigationLabel"))).click() ;
+                     System.out.println("Size of Size of Timeline: " + (Dimension) driver.findElement(By.xpath("//*[@id='u_0_a']/div[1]/div[1]/div/a/span")).getSize());
 
-wait.until(ExpectedConditions.presenceOfElementLocated(By.partialLinkText("Log Out"))).click();  
+                     System.out.println("Location of Size of Timeline: " + (Point) driver.findElement(By.xpath("//*[@id='u_0_a']/div[1]/div[1]/div/a/span")).getLocation());
+
+                   String friends = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[2]/ul/li[3]/a/span[1]"))).getText(); 
+                   
+                   System.out.println("Size of Size of Friends: " + (Dimension) driver.findElement(By.xpath("//div[2]/ul/li[3]/a/span[1]")).getSize());
+
+                   System.out.println("Location of Size of Friends: " + (Point) driver.findElement(By.xpath("//div[2]/ul/li[3]/a/span[1]")).getLocation());
+                   
+                   wait.until(ExpectedConditions.presenceOfElementLocated(By.id("userNavigationLabel"))).click() ;
+                   
+                   System.out.println("Size of Size of Account Settings: " + (Dimension) driver.findElement(By.id("userNavigationLabel")).getSize());
+
+                   System.out.println("Location of Size of Account Settings: " + (Point) driver.findElement(By.id("userNavigationLabel")).getLocation());
+
+                   wait.until(ExpectedConditions.presenceOfElementLocated(By.partialLinkText("Log Out"))).click();  
+                   
+                  System.out.println("Size of Size of Log Out: " + (Dimension) driver.findElement(By.partialLinkText("Log Out")).getSize());
+
+                   System.out.println("Location of Size of Log Out: " + (Point) driver.findElement(By.partialLinkText("Log Out")).getLocation());
 
                      driver.quit();
 
--- src/main/java/core/Safari.java
+++ src/main/java/core/Safari.java
@@ -18,8 +18,9 @@ public class Safari {
               logger.setLevel(Level.OFF);
              
               String url = "http://facebook.com/";
-              String email_address = "***************";
-              String password = "**************";
+              String email_address = "*******";
+
+              String password = "*******";
  
               if (!System.getProperty("os.name").contains("Mac")) {throw new IllegalArgumentException("Safari is available only on Mac");}
                     
@@ -39,19 +40,55 @@ public class Safari {
               
               String copyright = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\'pageFooter\']/div[3]/div/span"))).getText();
 
+              System.out.println("Size of copyright: " + (Dimension) driver.findElement(By.xpath("//*[@id=\'pageFooter\']/div[3]/div/span")).getSize());
+
+              System.out.println("Location of copyright: " + (Point) driver.findElement(By.xpath("//*[@id=\'pageFooter\']/div[3]/div/span")).getLocation());
               wait.until(ExpectedConditions.presenceOfElementLocated(By.id("email"))).clear();
 
               wait.until(ExpectedConditions.presenceOfElementLocated(By.id("email"))).sendKeys(email_address);
+              
+              System.out.println("Size of Email: " + (Dimension) driver.findElement(By.id("email")).getSize());
 
+              System.out.println("Location of Email: " + (Point) driver.findElement(By.id("email")).getLocation());
+              
               wait.until(ExpectedConditions.presenceOfElementLocated(By.id("pass"))).clear();
 
               wait.until(ExpectedConditions.presenceOfElementLocated(By.id("pass"))).sendKeys(password);
+              
+              System.out.println("Size of Size of Password: " + (Dimension) driver.findElement(By.id("pass")).getSize());
 
-              wait.until(ExpectedConditions.elementToBeClickable(By.id("u_0_2"))).click();
+              System.out.println("Location of Size of Password: " + (Point) driver.findElement(By.id("pass")).getLocation());
+
+              wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"u_0_2\"]"))).click();
+              
+              System.out.println("Size of Size of Login: " + (Dimension) driver.findElement(By.xpath("//*[@id=\"u_0_2\"]")).getSize());
+
+              System.out.println("Location of Size of Login: " + (Point) driver.findElement(By.xpath("//*[@id=\"u_0_2\"]")).getLocation());
 
               wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='u_0_a']/div[1]/div[1]/div/a/span"))).click();
 
-              String friends = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[2]/ul/li[3]/a/span[1]"))).getText();      
+              System.out.println("Size of Size of Timeline: " + (Dimension) driver.findElement(By.xpath("//*[@id='u_0_a']/div[1]/div[1]/div/a/span")).getSize());
+
+              System.out.println("Location of Size of Timeline: " + (Point) driver.findElement(By.xpath("//*[@id='u_0_a']/div[1]/div[1]/div/a/span")).getLocation());
+
+            String friends = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[2]/ul/li[3]/a/span[1]"))).getText(); 
+            
+            System.out.println("Size of Size of Friends: " + (Dimension) driver.findElement(By.xpath("//div[2]/ul/li[3]/a/span[1]")).getSize());
+
+            System.out.println("Location of Size of Friends: " + (Point) driver.findElement(By.xpath("//div[2]/ul/li[3]/a/span[1]")).getLocation());
+            
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.id("userNavigationLabel"))).click() ;
+            
+            System.out.println("Size of Size of Account Settings: " + (Dimension) driver.findElement(By.id("userNavigationLabel")).getSize());
+
+            System.out.println("Location of Size of Account Settings: " + (Point) driver.findElement(By.id("userNavigationLabel")).getLocation());
+
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.partialLinkText("Log Out"))).click();  
+            
+           System.out.println("Size of Size of Log Out: " + (Dimension) driver.findElement(By.partialLinkText("Log Out")).getSize());
+
+            System.out.println("Location of Size of Log Out: " + (Point) driver.findElement(By.partialLinkText("Log Out")).getLocation());
+      
         System.out.println("You have " + friends + " friends");
        
 //////////////////////////////////////////////////////////////////////////////////////////////////////
